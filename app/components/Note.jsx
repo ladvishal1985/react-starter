d6d8ae36879c4401db9dfc95efcb426de9dd610d
Vishal lad <ladvishal1985@gmail.com>
2016-04-20 14:44:45 +0530

    Basic React Todo with Add, Delete, Edit

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,3 +1,73 @@
 import React from 'react';
 
-export default () => <div>I have learnt Webpack - Cool! </div>;
+export default class Note extends React.Component{
+	constructor(props) {
+    	super(props);
+
+    	// Track `editing` state.
+    	this.state = {
+      		editing: false
+    	};
+  	}
+
+  	render() {
+  		if(this.state.editing) {
+      		return this.renderEdit();
+    	}
+
+    	return this.renderNote();
+  	}
+
+  	renderEdit = () => {
+  		return <input type="text"
+	      		ref={
+	        		element => element ?
+	        		element.selectionStart = this.props.task.length :
+	        		null
+	      		}
+	      		autoFocus={true}
+	      		defaultValue={this.props.task}
+	      		onBlur={this.finishEdit}
+	      		onKeyPress={this.checkEnter} />;
+  	};
+  	renderNote = () => {
+    	// If the user clicks a normal note, trigger editing logic.
+    	
+  		const onDelete = this.props.onDelete;
+  		return (
+	      <div onClick={this.edit}>
+	        <span className="task">{this.props.task}</span>
+	        {onDelete ? this.renderDelete() : null }
+	      </div>
+	    );
+  	};
+
+  	renderDelete = () => {
+	    return <button className="delete-note" onClick={this.props.onDelete}>x</button>;
+	};
+
+  	edit = () => {
+    	// Enter edit mode.
+    	this.setState({
+      		editing: true
+    	});
+  	};
+  	checkEnter = (e) => {
+  		// The user hit *enter*, let's finish up.
+    	if(e.key === 'Enter') {
+      		this.finishEdit(e);
+    	}
+  	};
+
+  	finishEdit = (e) => {
+  		const value = e.target.value;
+  		if(this.props.onEdit) {
+  		 	this.props.onEdit(value);
+		 	// Exit edit mode.
+  			this.setState({
+    			editing: false
+  			});
+  		}
+  	}
+}
+
